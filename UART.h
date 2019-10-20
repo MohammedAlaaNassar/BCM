9149f0ed3741a4d51c2060e0a988152f60441b18
Mohammed Nassar <mohammedalaanassar@hotmail.com>
2019-10-20 11:46:03 +0200

    fixes conflicts

--- UART.h
+++ UART.h
@@ -27,7 +27,7 @@ typedef struct
 typedef const UART_cfg_t* UART_cfgPtr_t;
 
 typedef uint8 UART_error_t;
-#define OK   0
+#define UART_OK            0
 #define UART_INVALID_CFG   1
 #define UART_PARITY_ERROR  2
 #define UART_FRAMING_ERROR 3
